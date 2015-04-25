f12602cca9388c171bce5c62586f37902e22f572
Samuel Fadel <samuelfadel@gmail.com>
2015-04-25 19:26:45 -0300

    NeRV R glue code ready to call C++ implementation.

--- R/nerv.R
+++ R/nerv.R
@@ -1,13 +1,16 @@
 #' Neighbor Retrieval Visualizer (NeRV)
 #'
-#' Creates a low-dimensional representation of high-dimensional data by
-#' optimizing user-defined costs and a tradeoff between mean smoothed precision
-#' and mean smoothed recall.
+#' Creates a k-dimensional representation of data by optimizing a trade-off
+#' between mean smoothed precision and mean smoothed recall.
 #'
 #' @param D A dist object or a symmetric distance matrix.
-#' @param lambda The tradeoff between mean smoothed precision and mean smoothed
+#' @param lambda The trade-off between mean smoothed precision and mean smoothed
 #'               recall.
-#' @return The low-dimensional representation of the data.
+#' @param Y Initial k-dimensional configuration. Random if omitted.
+#' @param k Target dimensionality.
+#' @param perplexity A rough upper bound on the neighborhood size.
+#' @param max.iter Maximum number of iteration the algorithm performs.
+#' @return The k-dimensional representation of the data.
 #'
 #' @references Venna, J.; Peltonen, J.; Nybo, K.; Aidos, H.; Kaski, S.,
 #'   "Information Retrieval Perspective to Nonlinear Dimensionality Reduction for
@@ -22,11 +25,28 @@
 #'
 #' @useDynLib mp
 #' @export
-nerv = function(D, lambda = 0.5) {
-  D = as.matrix(D)
+nerv <- function(D, lambda, Y=NULL, k=2, perplexity=20, max.iter=20) {
+  D <- as.matrix(D)
   if (!isSymmetric(D)) {
     stop("The distances matrix provided is not symmetric")
   }
 
-  # TODO
+  n <- nrow(D)
+  if (is.null(Y)) {
+    Y <- matrix(rnorm(n * k), ncol = k)
+  }
+
+  if (!is.matrix(Y)) {
+    Y <- as.matrix(Y)
+  }
+
+  if (ncol(Y) != k) {
+    stop("initial configuration does not match target dimensionality")
+  }
+
+  if (nrow(Y) != n) {
+    stop("initial configuration does not match dataset size")
+  }
+
+  .Call("mp_nerv", D, lambda, Y, k, perplexity, max.iter, Y, PACKAGE="mp")
 }
