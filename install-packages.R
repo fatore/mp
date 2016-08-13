7704b88f972f10bffe7451e770763bf4a3ce7f2c
Francisco Fatore <francisco.fatore@realimpactanalytics.com>
2016-08-13 21:18:06 +0200

    Add dependencies management

--- install-packages.R
+++ install-packages.R
@@ -1,8 +1,14 @@
-## Create the personal library if it doesn't exist. Ignore a warning if the directory already exists.
+packages = c(
+                "roxygen2",
+                "Rcpp",
+                "RcppArmadillo",
+                "proxy"
+            )
+
+
+# Create the personal library if it doesn't exist. Ignore a warning if the directory already exists.
 dir.create(Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)
 
-install.packages(c("roxygen2", "RcppArmadillo", "proxy"), Sys.getenv("R_LIBS_USER"), repos = "http://cran.case.edu" )
-
-## Install a package that you have copied to the remote system.
-## install.packages("file_name.tar.gz", Sys.getenv("R_LIBS_USER")
+# Install listed packages
+sapply(packages, install.packages, Sys.getenv("R_LIBS_USER"))
 
